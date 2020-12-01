cb7ba5f7d666cf184e61419e6ba6dc257dd80c73
merzs1 <susanne.merz@aalto.fi>
2020-12-01 13:05:46 +0200

    changed code to solve second part, instead of first

--- ReportRepair.py
+++ ReportRepair.py
@@ -3,10 +3,19 @@ with open('Report.csv') as fi:
     for line in fi:
         numbers.add(int(line.strip(' ')))
     fi.close()
-
-for n in numbers:
-    test = 2020 - n
-    if (test in numbers):
-        target = test
-res = target * (2020-target)
+sorted_numbers = sorted(numbers)
+smallest=sorted_numbers[0]
+for n in reversed(sorted_numbers):
+    test1 = 2020 - n
+    for m in sorted_numbers:
+        test2=test1-m
+        if (test2 < smallest):
+            break
+        if (test2 in numbers):
+            target1 = test2
+            target2 = m
+            target3 = n
+res = target1 * target2 * target3
 print(res)
+
+
