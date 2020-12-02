f5cb14b810d03018008c63138cbc4af2f5aae8a4
merzs1 <susanne.merz@aalto.fi>
2020-12-02 10:54:43 +0200

    second day, second puzzle added

--- AoC2020_D2.py
+++ AoC2020_D2.py
@@ -1,13 +1,18 @@
 import re
-counter=0
+counter = 0
+counter_part2 = 0
 with open('input2') as fi:
     for line in fi:
         (conditions, passwd) = line.split(':')
         (numbers, testchar)=conditions.split(' ')
         (lower,upper)= numbers.split("-")
+        print(passwd[int(lower)+1])
+        if((passwd[int(lower)]==testchar) ^ (passwd[int(upper)]==testchar)):
+            counter_part2 = counter_part2+1
         numchars = re.findall(testchar,passwd)
         if(len(numchars)<int(lower) or len(numchars) >int(upper)):
             continue
         counter= counter+1
-    print(counter)
+    print(f"first part result is {counter}")
+    print(f"second part result is {counter_part2}")
     fi.close()
