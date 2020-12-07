902044b047a782e637c313fac81005557bb61ac0
merzs1 <susanne.merz@aalto.fi>
2020-12-08 00:58:24 +0200

    added part 2 of day7

--- AoC2020_D7.py
+++ AoC2020_D7.py
@@ -1,12 +1,27 @@
 import re
+
+def count_bags(node_name):
+    curr_node = baggage_rules[node_name]
+    out = 0
+    if 'other' in curr_node:
+        return 0
+    for c,num in curr_node.items():
+        out += count_bags(c) * int(num) + int(num)
+    return out
+
+
 with open('input_D7') as fi:
     baggage_rules = dict()
     for line in fi:
         entry = line.split('contain')
-        key = re.sub('bags+', '', entry[0]).strip()
-        vals = re.sub('\d | bags| bag','',entry[1]).strip('. \n')
+        key = re.sub('bags', '', entry[0]).strip()
+        vals = re.sub(' bags| bag', '', entry[1]).strip('. \n')
         vals = vals.split(', ')
-        baggage_rules[key]=set(vals)
+        values = dict()
+        for v in vals:
+            num, color = v.split(' ', maxsplit=1)
+            values[color] = num
+        baggage_rules[key]=values
     #print(baggage_rules)
 
     bag_colors = set()
@@ -22,4 +37,13 @@ with open('input_D7') as fi:
                         new_colors.add(node)
         bag_colors = bag_colors.union(current_cols)
     print(f"part1 is {len(bag_colors)-1}")
+
+    # assuming that there is no circularity in the subtree we are traversing,
+    # because otherwise this will go, well you know where
+    root_col = 'shiny gold'
+    num_bags= count_bags(root_col)
+
+    print(f"part2 is {num_bags}")
+
+
 
